484b4901050fcdeee7eaa85bcc17edb8d1d3445f
Abraham325 <jamesis325@gmail.com>
2022-09-27 09:35:23 +0100

    create new file

--- app.js
+++ app.js
@@ -34,6 +34,6 @@ app.use(bodyParser.urlencoded({extended: true}));
  })
 
 
-app.listen(3000, function(req, res){
+app.listen(process.env.PORT || 3000, function(req, res){
     console.log("server is running on port 3000");
 });
